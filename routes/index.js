122b6b3477740a78da740d8fcbe0c9f5d722a517
MOHAMMAD KHAN <usman.khan@yahoo.com>
2019-01-04 20:31:29 +0500

    admin side 3

--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,7 @@ router.get('/login', function(req, res, next){
   res.render('login', {layout: 'abc'} );
 });
 router.get('/test', function(req, res, next){
-  res.render('admin');
+  res.render('admin' , {layout: 'admin'});
 });
 
 router.get('/signup', function(req, res, next){
