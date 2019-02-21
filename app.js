e29ac3ec10e9e1df3d6a9f36da9228974f19e387
zaki abbasi chotu lapi <zakiabbasi15@yahoo.com>
2019-02-21 11:34:56 +0500

    updated aws keys

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const session = require('express-session');
 const passport = require('./config/passport');
 const busboyBodyParser = require('busboy-body-parser');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 const mongoose = require('mongoose');
